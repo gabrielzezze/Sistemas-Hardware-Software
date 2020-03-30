98c01f112caa97b880a8d58739c6d0f2bcb4a1d4
gabriel zezze <gabrielzezze@me.com>
2020-03-30 11:30:46 -0300

    Atividades Aula 7 Done

--- 07-loops/solucao_ex4.c
+++ 07-loops/solucao_ex4.c
@@ -4,3 +4,13 @@
  * 
  * Chame sua função de soma_n_solucao */
 
+
+long soma_n_solucao(int a) {
+  int cont = 0; // edx
+  long ret = 0; // eax
+  while(cont < a) {
+    ret += (long) cont;
+    cont ++;
+  }
+  return ret;
+}
--- 07-loops/solucao_ex5.c
+++ 07-loops/solucao_ex5.c
@@ -4,3 +4,14 @@
  * 
  * Chame sua função de ex5_solucao */
  
+long ex5_solucao(long a, long b) {
+  long ecx = 0;
+  long r8 = 0;
+  long ret;
+  while(ecx < a) {
+    long div = ecx%b;
+    if(div == 0) r8 += ecx;
+    ecx += 1;
+  }
+  return r8;
+}
